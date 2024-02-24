91b9d3b376c765e0cbcc1b54b35e9d0b666f50b5
Rahmat Ullah <rullah0202@gmail.com>
2024-02-24 22:34:14 +0600

    Last update 10PM 24-02-24

--- components/AuthOverlay.vue
+++ components/AuthOverlay.vue
@@ -3,7 +3,7 @@
         id="AuthOverlay" 
         class="fixed flex items-center justify-center z-50 top-0 left-0 w-full h-full bg-black bg-opacity-50"
     >
-        <div class="relative bg-white w-full max-w-[470px] h-[70%] p-4 rounded-lg">
+        <div class="relative bg-white w-full max-w-[470px] h-[80%] p-4 rounded-lg">
 
             <div class="w-full flex justify-end">
                 <button 
