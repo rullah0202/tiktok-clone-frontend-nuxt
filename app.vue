2017bbe52860b646309489dd7bcefa0f16d391d9
Rahmat Ullah <rullah0202@gmail.com>
2024-02-24 14:24:28 +0600

    Last Update 2 PM 24-02-24

--- app.vue
+++ app.vue
@@ -10,6 +10,10 @@ import { storeToRefs } from 'pinia';
 const { $userStore, $generalStore } = useNuxtApp()
 const { isLoginOpen, isEditProfileOpen } = storeToRefs($generalStore)
 
+useSeoMeta({
+    title: 'Tiktok Clone'
+})
+
 onMounted(async () => {
     $generalStore.bodySwitch(false) 
     isLoginOpen.value = false
